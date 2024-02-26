2c82e3cbf66b7fe9e43cc4aae880ef40b9c76d49
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2024-02-26 09:39:15 +0900

    feat: about get function add

---
+++ src/lib/newt/get-about.ts
@@ -0,0 +1,13 @@
+import { newtClient } from '@/lib/newt/newt-client'
+import type { about as about_type } from '@/types/data'
+
+export const getAbout = async () => {
+  const { items: about } = await newtClient.getContents<about_type>({
+    appUid: 'document-site',
+    modelUid: 'about',
+    query: {
+      select: ['name', 'position', 'content']
+    }
+  })
+  return about
+}
