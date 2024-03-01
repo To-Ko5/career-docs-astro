c4b1800c6b9f979d4ad2df4ac091143bfb458ae9
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2024-03-01 09:42:21 +0900

    refactor: github icon use

--- data/site_data.ts
+++ data/site_data.ts
@@ -1,4 +1,4 @@
-import { Link, BookText } from 'lucide-react'
+import { Github, BookText } from 'lucide-react'
 import type { url_link } from 'types/data'
 
 export const site = {
@@ -12,7 +12,8 @@ export const date = {
 
 export const urlLinks: url_link[] = [
   {
-    icon: Link,
+    // Deprecated but temporarily used
+    icon: Github,
     href: import.meta.env.LINK_URL_1 || '#',
     tooltip: 'Github'
   },
