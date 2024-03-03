ce41547e3f7018b7d48f3e0ed8dee516f88ce0fa
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2024-03-03 10:18:58 +0900

    Revert "feat: site config add"
    This reverts commit a564a9ac2e3a8d51df11ceb8229eb08715b4ed3a.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,6 @@ import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://to-ko5.github.io',
-  base: '/docs',
   integrations: [
     react(),
     tailwind({
