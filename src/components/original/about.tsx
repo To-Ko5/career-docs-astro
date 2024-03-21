f74689eb42e973ba379a8babcf4f3d294c555808
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2024-03-21 11:36:28 +0900

    feat: line-height style add

--- src/components/original/about.tsx
+++ src/components/original/about.tsx
@@ -6,7 +6,7 @@ const About = ({ content }: Props) => {
   return (
     <section>
       <h2 className="text-xl font-bold mb-2">About</h2>
-      <p className="whitespace-pre-line text-muted-foreground print:text-sm">
+      <p className="whitespace-pre-line text-muted-foreground leading-[1.6rem] print:text-sm">
         {content}
       </p>
     </section>
